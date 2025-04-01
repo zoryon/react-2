343c03e00b3e90b82fdcf17c82be7d9902e342db
Claudio Benvenuti <claudio@benve.it>
2025-04-01 20:56:27 +0200

    aggiunte certificazioni

--- build/init.sql
+++ build/init.sql
@@ -1,15 +1,25 @@
 CREATE TABLE `alunni` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(20) NOT NULL,
-  `cognome` varchar(20) NOT NULL
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(20) NOT NULL,
+  `cognome` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `alunni` (`id`, `nome`, `cognome`) VALUES
-(1, 'claudio', 'benve'),
-(2, 'ivan', 'bruno');
+CREATE TABLE `certificazioni` (
+  `id` INT(11) NOT NULL AUTO_INCREMENT,
+  `alunno_id` INT(11) NOT NULL,
+  `titolo` VARCHAR(100) NOT NULL,
+  `votazione` INT(3) NOT NULL CHECK (`votazione` BETWEEN 0 AND 100),
+  `ente` VARCHAR(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`alunno_id`) REFERENCES `alunni`(`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-ALTER TABLE `alunni`
-ADD PRIMARY KEY (`id`);
+INSERT INTO `alunni` (`nome`, `cognome`) VALUES
+('claudio', 'benve'),
+('ivan', 'bruno');
 
-ALTER TABLE `alunni`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+INSERT INTO `certificazioni` (`alunno_id`, `titolo`, `votazione`, `ente`) VALUES
+(1, 'Certificazione Python', 85, 'Coursera'),
+(1, 'Certificazione SQL', 90, 'Udemy'),
+(2, 'Certificazione Java', 78, 'Oracle');
